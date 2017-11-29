2d36903e9c674c623cb74b6283351f664e1267fe
Beles <maxbel84@gmail.com>
2017-11-29 23:39:16 +0600

    Add project files.

---
+++ pyapp2/pyapp2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import radians
+def main():
+    x=np.arange(0, radians(1800), radians(12))
+    plt.plot(x, np.cos(x), 'b')
+    plt.show()
+
+main()
+
